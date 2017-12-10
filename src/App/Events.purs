3690041e5583136072325d1f027a97df2a489689
Daniel Bachler <daniel@danielbachler.de>
2017-12-10 20:56:14 +0100

    Attempt to use Simple.JSON for json decoding
    Does not compile, Newtyped records seem to be a problem?

--- src/App/Events.purs
+++ src/App/Events.purs
@@ -2,21 +2,57 @@ module App.Events where
 
 import Prelude
 import App.Routes (Route)
-import App.State (State(..))
+import App.State (State(..)) 
 import Data.Maybe
 import Data.Function (($))
-import Network.HTTP.Affjax (AJAX)
+import Network.HTTP.Affjax (AJAX, get, AffjaxResponse)
 import Pux (EffModel, noEffects)
 import Control.Monad.Aff.Console (log, CONSOLE)
+import Control.Monad.Aff (attempt) 
+import Control.Monad.Except(runExcept)
+import Control.Monad.Eff.Exception (Error)
+import Control.Monad.Trans.Class (lift)
+import Data.List (List)
+import Data.Either (Either(Left, Right), either)
+import App.State (Collaborator)
+import Simple.JSON (readJSON) 
+import Data.Foreign.Generic (genericDecodeJSON)
+import Data.Foreign (F, ForeignError) 
+import Data.List.Types (NonEmptyList)
+
+data CombinedError
+  = NetworkError Error
+  | DecodeError (NonEmptyList ForeignError) 
 
 data Event
   = PageView Route
   | ShowSecret
+  | RequestCollaborators
+  | ReceiveCollaborators (Either (CombinedError) (Array Collaborator))
 
 type AppEffects fx = (ajax :: AJAX, console :: CONSOLE | fx)
 
 foldp :: ∀ fx. Event -> State -> EffModel State Event (AppEffects fx)
-foldp (PageView route) (State st) = noEffects $ State st { route = route, loaded = true }
+foldp (PageView route) (State st) = noEffects $ State st { route = route, loaded = true } 
+
 foldp (ShowSecret) (State st) = { state: State st { title = "Secret title!"},
-                                  effects: [ log "Secret button clicked" *> pure Nothing ]
+                                  effects: [ log "Secret button clicked" *> pure Nothing ] 
                                 }
+
+foldp (RequestCollaborators) (State st) =
+  { state: State st { title = "Fetching actors and movies..." }
+  , effects: [ do
+      eitherRes :: Either Error (AffjaxResponse String) <- attempt $ get "http://localhost:4000/collaborator?select=*"
+      
+      let collaborators = case eitherRes of 
+                                Left err -> Left $ NetworkError err
+                                Right res -> either (\decodeErr -> Left $ DecodeError decodeErr) (\collaborators -> Right collaborators) (readJSON (res.response) :: Either (NonEmptyList ForeignError) (Array Collaborator))
+      
+      pure $ Just $ ReceiveCollaborators collaborators 
+    ]
+  }
+
+foldp (ReceiveCollaborators collaboratorsOrErrors) (State st) =
+  noEffects $ case collaboratorsOrErrors of
+    Left err -> State st { title = "Error fetching todos: " <> show err }
+    Right collaborators -> State st { collaborators = collaborators, title = "Collaborators fetched" }
--- src/App/State.purs
+++ src/App/State.purs
@@ -2,20 +2,32 @@ module App.State where
 
 import App.Config (config)
 import App.Routes (Route, match)
+import Data.Foreign.Class (class Decode)
+import Data.Foreign.Generic (genericDecode, defaultOptions)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
+import Data.Lens (_1)
 import Data.Newtype (class Newtype)
 import Data.Show (class Show)
+import Prelude (($))
+import Simple.JSON (class ReadForeign, readImpl)
+import Data.Newtype (class Newtype, unwrap) 
+--import Data.Argonout (class DecodeJson, decodeJson, (.?))
 
-newtype State = State
-  { title :: String
+newtype State = State StateRecord
+type StateRecord =
+  { title :: String 
   , route :: Route
   , loaded :: Boolean
+  , collaborators :: Array Collaborator
+  , persons :: Array Person
+  , movies :: Array Movie
   }
 
 newtype MovieId = MovieId String
 
-newtype Movie = Movie
+newtype Movie = Movie MovieRecord
+type MovieRecord =
   { id :: MovieId
   , title :: String
   , year :: Int
@@ -23,26 +35,99 @@ newtype Movie = Movie
 
 newtype PersonId = PersonId String
 
-newtype Person = Person
+newtype Person = Person PersonRecord
+type PersonRecord =
   { id :: PersonId
   , firstName :: String
   , lastName :: String
   }
 
-newtype Collaborator = Collaborator
-  { personId :: PersonId
+newtype CollaboratorId = CollaboratorId String
+
+newtype Collaborator = Collaborator CollaboratorRecord
+type CollaboratorRecord =
+  { id :: CollaboratorId
+  , personId :: PersonId
   , movieId :: MovieId
-  , role :: String
   }
 
-derive instance genericState :: Generic State _
-derive instance newtypeState :: Newtype State _
+derive instance ntMovieId :: Newtype MovieId _
+derive instance ntPersonId :: Newtype PersonId _
+derive instance ntCollaboratorId :: Newtype CollaboratorId _
+derive instance ntMovie :: Newtype Movie _
+derive instance ntPerson :: Newtype Person _
+derive instance ntCollaborator :: Newtype Collaborator _
+derive instance ntState :: Newtype State _
 
-instance showState :: Show State where show = genericShow
+derive newtype instance showMovieId :: Show MovieId
+derive newtype instance showPersonId :: Show PersonId
+derive newtype instance showCollaboratorId :: Show CollaboratorId
 
-init :: String -> State
+init :: String -> State 
 init url = State
   { title: config.title
   , route: match url
   , loaded: false
+  , collaborators : []
+  , persons : []
+  , movies : []
   }
+
+{-
+derive instance genericMovie :: Generic Movie _
+derive instance genericMovieId :: Generic MovieId _
+derive instance genericPerson :: Generic Person _
+derive instance genericPersonId :: Generic PersonId _
+derive instance genericCollaborator :: Generic Collaborator _
+derive instance genericCollaboratorId :: Generic CollaboratorId _
+derive instance genericState :: Generic State _
+
+instance showMovie :: Show Movie where show = genericShow
+instance showPerson :: Show Person where show = genericShow
+instance showCollaborator :: Show Collaborator where show = genericShow
+instance showState :: Show State where show = genericShow
+-}
+
+derive newtype instance readFMovieId :: ReadForeign MovieId
+derive newtype instance readFPersonId:: ReadForeign PersonId
+derive newtype instance readFCollaboratorId :: ReadForeign CollaboratorId
+
+{-
+derive instance readFPersonRecord :: ReadForeign PersonRecord
+derive instance readFMovieRecord :: ReadForeign MovieRecord
+derive instance readFCollaboratorRecord :: ReadForeign CollaboratorRecord
+
+
+
+derive newtype instance readFMovie :: ReadForeign Movie
+derive newtype instance readFPerson :: ReadForeign Person
+-}
+--derive newtype instance readFCollaborator :: ReadForeign Collaborator
+
+--derive newtype instance readFState :: ReadForeign State
+
+
+{--
+instance decodeMovieId :: Decode MovieId where
+  decode = genericDecode $ defaultOptions {unwrapSingleConstructors = true}
+
+instance decodeMovie :: Decode Movie where
+  decode = genericDecode $ defaultOptions {unwrapSingleConstructors = true}
+
+instance decodePersonId :: Decode PersonId where
+  decode = genericDecode $ defaultOptions {unwrapSingleConstructors = true}
+
+instance decodePerson :: Decode Person where
+  decode = genericDecode $ defaultOptions {unwrapSingleConstructors = true}
+
+instance decodeCollaboratorId :: Decode CollaboratorId where
+  decode = genericDecode $ defaultOptions {unwrapSingleConstructors = true}
+
+instance decodeCollaborator :: Decode Collaborator where
+  decode = genericDecode $ defaultOptions {unwrapSingleConstructors = true}
+
+instance decodeState :: Decode State where
+  decode = genericDecode $ defaultOptions {unwrapSingleConstructors = true}
+--}
+
+
